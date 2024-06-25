dad6e5976fc9b3e6ca0e3e2214960a559d453518
rinterron <144239782+ssmitee@users.noreply.github.com>
2024-06-26 00:41:49 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        var colors = new Dictionary<string, Dictionary<string, string>>
+        {
+            {
+                "черный", new Dictionary<string, string>
+                {
+                    {"name", "black"},
+                    {"rgb10", "rgb(0, 0, 0)"},
+                    {"rgb16", "#000000"}
+                }
+            },
+            {
+                "серый", new Dictionary<string, string>
+                {
+                    {"name", "grey"},
+                    {"rgb10", "rgb(128, 128, 128)"},
+                    {"rgb16", "#808080"}
+                }
+            },
+            {
+                "белый", new Dictionary<string, string>
+                {
+                    {"name", "white"},
+                    {"rgb10", "rgb(255, 255, 255)"},
+                    {"rgb16", "#FFFFFF"}
+                }
+            }
+        };
+
+        var greyColor = colors["серый"];
+
+        Console.WriteLine($"color: {greyColor["name"]};");
+        Console.WriteLine($"color: {greyColor["rgb10"]};");
+        Console.WriteLine($"color: {greyColor["rgb16"]};");
+    }
+}
